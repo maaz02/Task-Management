b5541d66d35e8327c600a415568e216c51b16504
maaz02 <shaikhmaaz11@gmail.com>
2021-09-29 00:26:36 +0530

    Basic Functions added

---
+++ index.js
@@ -0,0 +1,31 @@
+const taskContainer = document.querySelector(".task__container");
+
+
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`,
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value,
+  };
+
+  const newCard = `
+  <div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}>
+    <div class="card">
+      <div class="card-header d-flex justify-content-end gap-2">
+        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+      </div>
+      <div class="card-body">
+        <img src=${taskData.imageUrl} class="card-img-top" alt="...">
+        <h5 class="card-title mt-3 fw-bold text-primary">${taskData.taskTitle}</h5>
+        <p class="card-text">${taskData.taskDescription}</p>
+        <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+      </div>
+    </div>
+  </div>
+  `;
+
+  taskContainer.insertAdjacentHTML("beforeend", newCard);
+};
