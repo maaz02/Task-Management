58af2aade71ebccc261cea22baaaf28e6b86fa43
maaz02 <shaikhmaaz11@gmail.com>
2021-09-29 12:38:54 +0530

    Added delete function

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const taskContainer = document.querySelector(".task__container");
-
-
-const saveChanges = () => {
-  const taskData = {
-    id: `${Date.now()}`,
-    imageUrl: document.getElementById("imageurl").value,
-    taskTitle: document.getElementById("tasktitle").value,
-    taskType: document.getElementById("tasktype").value,
-    taskDescription: document.getElementById("taskdescription").value,
-  };
-
-  const newCard = `
-  <div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}>
+let globalStore = []; //Array of objects
+const generateNewCard = (taskData) =>
+  `
+  <div class="col-sm-12 col-md-6 col-lg-4">
     <div class="card">
       <div class="card-header d-flex justify-content-end gap-2">
         <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"><i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
       </div>
       <div class="card-body">
         <img src=${taskData.imageUrl} class="card-img-top" alt="...">
@@ -27,5 +18,62 @@ const saveChanges = () => {
   </div>
   `;
 
-  taskContainer.insertAdjacentHTML("beforeend", newCard);
+const loadInitialCardData = () => {
+  //localstorage to get tasky card taskdata
+  const getCardData = localStorage.getItem("tasky");
+  //convert to normal object
+  const {cards} = JSON.parse(getCardData);
+  //loop over those array of task object to create HTML card, inject it to DOM
+  cards.map((cardObject) => {
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+
+    //update our globalStore
+    globalStore.push(cardObject);
+  }
+
+ )
+
 };
+
+//Delete function
+
+const deleteCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  globalStore = globalStore.filter((cardObject) =>  cardObject.id !== targetID);
+  localStorage.setItem("tasky",JSON.stringify({cards: globalStore}));
+
+  if(tagname === "BUTTON") {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  } else {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  };
+};
+
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`,
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value,
+  };
+
+
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+
+  globalStore.push(taskData);
+  localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+};
+
+
+// Issues
+
+// Page refreshes causes the data to get deleted
+// API -> Application Programming Interface
+// local storage -> Accessing application via local storage
+//Interface -> Inteface means middle man
+
+// Features - Delete, edit, open the card
